30cb4a379a30dfd3bb2b7a8f400259c261c63013
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-10 16:51:09 -0700

    added code from my_first_io.js, for reference

--- my_first_async_io.js
+++ my_first_async_io.js
@@ -4,3 +4,10 @@ var buff = Buffer.from(fs.readFileSync(process.argv[2]))
 	.toString("utf8").split("\n").length - 1;
 
 console.log(buff);
+
+
+// var fs = require('fs')
+//
+// var contents = fs.readFileSync(process.argv[2])
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
