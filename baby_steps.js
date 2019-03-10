919017de7b577a874c714dfd6bf238682e9c67ee
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-10 16:46:32 -0700

    added official solution, for reference

--- baby_steps.js
+++ baby_steps.js
@@ -6,3 +6,12 @@ for (var i = 2; i < userInput.length; i++) {
 }
 
 console.log(sum);
+
+//official solution for reference
+// var result = 0
+//
+// for (var i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i])
+// }
+//
+// console.log(result)
