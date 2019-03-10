cac730dbeaa09720ee6b1ed4432cf8d69b917ceb
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-10 16:44:14 -0700

    exercise file, used to test code

--- program.js
+++ program.js
@@ -1,6 +1,13 @@
 var fs = require("fs");
 
-var buff = Buffer.from(fs.readFile(process.argv[2]))
+var buff = Buffer.from(fs.readFileSync(process.argv[2]))
 	.toString("utf8").split("\n").length - 1;
 
 console.log(buff);
+
+//official solution for reference
+// var fs = require('fs')
+//
+// var contents = fs.readFileSync(process.argv[2])
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
