d47ae6276b9678748c03b250ae82ed8a91da4d04
Jeremy Westfall <jeremydwestfall@gmail.com>
2019-03-11 20:42:11 -0700

    coudn't get code to work

--- my_first_async_io.js
+++ my_first_async_io.js
@@ -1,11 +1,24 @@
 var fs = require("fs");
 
-var buff = Buffer.from(fs.readFileSync(process.argv[2]))
-	.toString("utf8").split("\n").length - 1;
+var answer = fs.readFile(process.argv[2], function (err, data) {
+	var dataRead = data.toString("utf8").split("\n").length - 1;
+	return dataRead;
+}
 
-console.log(buff);
 
+console.log(answer);
 
+// not sure why this isn't working, I would think it would 
+
+// my solution to my_first_io.js
+// var fs = require("fs");
+//
+// var buff = Buffer.from(fs.readFileSync(process.argv[2]))
+// 	.toString("utf8").split("\n").length - 1;
+//
+// console.log(buff);
+
+//official solution for reference
 // var fs = require('fs')
 //
 // var contents = fs.readFileSync(process.argv[2])
